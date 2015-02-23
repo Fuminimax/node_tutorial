2726522f7aea180f5dd307976bec2f3e02985fd9
fumio <fumio@fumio-virtual-machine>
2015-02-23 16:53:47 +0900

    Node.jsのサンプル作成

---
+++ node_tutorial/TwitterSample.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: 'QFOiO0R9A1oqK94UnjI3Q',
+	consumer_secret: '9lC3I6cBBNK4TLo6vZFHKpcunJs1FZVGxMAOow8ucs',
+	access_token_key: '16167279-9NU9kRgh7Len6teTRax78vlxa3lQc3urgNkzsa8PC',
+	access_token_secret: '4QitfT0CoQVMynWyQdC7UMyYWynMheMxMNF6PPD48'
+});
+
+
+
